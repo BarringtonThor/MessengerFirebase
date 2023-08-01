3016a7607fef87980ddc7722066b5f4c44fb02e2
Barry Thorpe <Barringtonthorpe@gmail.com>
2023-07-31 23:43:58 -0500

    Add files via upload

--- MessengerFirebase/MessengerFirebase/Core/Profile/View/ProfileView.swift
+++ MessengerFirebase/MessengerFirebase/Core/Profile/View/ProfileView.swift
@@ -6,17 +6,32 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ProfileView: View {
+    @StateObject var viewModel = ProfileViewModel()
+    let user: User
+    
     var body: some View {
         VStack {
             VStack {
-                Image(systemName: "person.circle.fill")
-                    .resizable()
-                    .frame(width: 80, height: 80)
-                    .foregroundColor(Color(.systemGray4))
+                PhotosPicker(selection: $viewModel.selectedItem) {
+                    if let profileImage = viewModel.profileImage {
+                        profileImage
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                    } else {
+                        Image(user.profileImageUrl ?? "")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                    }
+                }
                 
-                Text("Bruce Wayne")
+                Text(user.fullname)
                     .font(.title2)
                     .fontWeight(.semibold)
             }
@@ -53,6 +68,6 @@ struct ProfileView: View {
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileView()
+        ProfileView(user: User.MOCK_USER)
     }
 }
